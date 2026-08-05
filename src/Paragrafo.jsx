6191e83bffbe0c0a449b25183fe1827a857794fe
João Rafael Endrizzi <joaoendrizzi@gmail.com>
2026-08-05 11:18:05 -0300

    First Commit
    .

---
+++ src/Paragrafo.jsx
@@ -0,0 +1,9 @@
+import images from './assets/images.jpg';
+
+function Paragrafo() {
+    return (
+            <p>Paragrafo explicatvo
+            <img src={images} width={150} height={150} /></p>
+    );
+}
+export default Paragrafo;
